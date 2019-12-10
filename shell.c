3f78c6d1af57c1f2512d62d1b0940fc7655850cc
rleongPd5 <rleong00@stuy.edu>
2019-12-10 05:20:49 -0500

    tried changing the order so a token isn't lost

--- shell.c
+++ shell.c
@@ -50,17 +50,18 @@ char ** parse_args( char * line , char * separator, int size ){
     // pointers[i] = malloc(strlen(line)+1);    // allocate desired memory to each pointer
     token = strsep(&curr, separator);
     // printf("token: %s |curr: %s\n", token, curr);
+    // Returns the beginning of the original string,
+    // sets source to the string starting at 1 index past the location of the new NULL
+    pointers[i] = token;
     if (!strcmp(token, "cd")){
       // printing current working directory
       printf("%s\n", getcwd(s, 100));
       token = strsep(&curr, separator); //takes next arg for cd
+      pointers[i] = token;
       chdir(token);
     } else if (!strcmp(token, "exit")){
       exit(0);
     }
-    // Returns the beginning of the original string,
-    // sets source to the string starting at 1 index past the location of the new NULL
-    pointers[i] = token;
     // printf("iteration %d | curr: %s | token: %s\n", i, curr, token);
     i++;
   }
