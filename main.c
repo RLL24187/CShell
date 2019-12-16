c43cc51a294e759e6b9b82350899e44bd6127c49
rleongPd5 <rleong00@stuy.edu>
2019-12-16 05:48:29 -0500

    print statements

--- main.c
+++ main.c
@@ -20,19 +20,19 @@ int main(){
   char ** semiargs;
   // separator = " ";
   while (1){
-
+    printf("Hello there! Welcome to my shell. Not the best but I tried :)\n");
     printf("shell $: ");
     fgets(line, 200, stdin); //adds a newline to the end of line
     line[strlen(line) - 1] = '\0';
-    printf("\tParsing with ';'...\n");
+    // printf("\tParsing with ';'...\n");
     // parse using ;
     semiargs = parse_args(line, ";", count_tokens(line, ";"));
     int i = 0;
-    printf("\tParsing with ' '...\n");
+    // printf("\tParsing with ' '...\n");
     while (semiargs[i]){
-      printf("\t\tParsing semiargs[%d]...\n", i);
+      // printf("\t\tParsing semiargs[%d]...\n", i);
       args = parse_args(semiargs[i], " ", count_tokens(semiargs[i], " ")); //parses based on spaces
-      printf("\t\tRunning args from semiargs[%d]...\n", i);
+      // printf("\t\tRunning args from semiargs[%d]...\n", i);
       execArgs(args);
       i++;
     }
