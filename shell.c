1936ca5fd3b9f57d481a2b822f3ca4e12f18e283
rleongPd5 <rleong00@stuy.edu>
2019-12-16 04:26:58 -0500

    maybe it's not null terminated?

--- shell.c
+++ shell.c
@@ -44,7 +44,7 @@ char ** parse_args( char * line , char * separator, int size ){
   // char s[100]; //to print working directory
   // printf("line: %s\n", line);
   // printf("size line: %d\n", size);
-  char ** pointers = malloc((size + 1) * sizeof(char *)); //allocate memory for 5 pointers (b/c at most 5 args) + make room for NULL
+  char ** pointers = calloc((size + 1), sizeof(char *)); //allocate memory for 5 pointers (b/c at most 5 args) + make room for NULL
   int i = 0;
   while (curr){
     // printf("iteration %d | curr: %s | token: %s\n", i, curr, token);
